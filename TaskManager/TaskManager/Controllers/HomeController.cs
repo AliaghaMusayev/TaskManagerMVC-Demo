c42599e11dab75abfbc2c6c513c0b86ff0c0bf82
AliaghaMusayev <aliaghamusayev@gmail.com>
2018-03-30 15:52:06 +0400

    Create Task and modify added

--- TaskManager/TaskManager/Controllers/HomeController.cs
+++ TaskManager/TaskManager/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TaskManager.Models;
+using TaskManager.Models.DBModel;
 using TaskManager.ViewModels;
 
 namespace TaskManager.Controllers
@@ -10,6 +12,7 @@ namespace TaskManager.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        MyMainDBModel dbConnection = new MyMainDBModel();
         public ActionResult Admin()
         {
             if (Convert.ToBoolean(Session["loggedUser"])==false)
@@ -21,7 +24,7 @@ namespace TaskManager.Controllers
             {
                 ViewBag.userName = selectedData[0].selectedUsers.UserName;
             }
-            
+
             return View(selectedData);
         }
 
@@ -39,10 +42,30 @@ namespace TaskManager.Controllers
             return View(selectedData);
         }
 
-
         public ActionResult Modify()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Modify(Tasks modifiedTask, int Id)
+        {
+            dbConnection.Tasks.FirstOrDefault(f => f.Id == Id).Status=modifiedTask.Status;
+            dbConnection.SaveChanges();
+            return View();
+        }
+
+        public ActionResult AddTask()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddTask(Tasks newTask)
+        {
+            dbConnection.Tasks.Add(newTask);
+            dbConnection.SaveChanges();
+            return View("Admin");
         }
     }
 }
